6db9ac2ffb6315142add10fecd08c1aaf82d6929
13angs <13angz.um@gmail.com>
2023-12-22 16:06:07 +0000

    Fix SPA proxy is not ready. Returning temporary landing page

--- Program.cs
+++ Program.cs
@@ -11,12 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
-
 var configuration = builder.Configuration;
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,7 +26,7 @@ builder.Services.AddCors(options =>
                 .AllowAnyMethod();
             });
         });
-builder.Services.AddControllers()
+builder.Services.AddControllersWithViews()
     .AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,12 +1,18 @@
+using Newtonsoft.Json;
+
 namespace OmniChat;
 
 public class WeatherForecast
 {
+    [JsonProperty("date")]
     public DateOnly Date { get; set; }
 
+    [JsonProperty("temperaturec")]
     public int TemperatureC { get; set; }
 
+    [JsonProperty("temperaturef")]
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    [JsonProperty("summary")]
     public string? Summary { get; set; }
 }
